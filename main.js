d40206767f404ed5634b353922d3f6064e13c28c
anheca <anheca68@gmail.com>
2021-05-11 18:56:06 -0300

    se incluyo ventana modal

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import init from './src/lib/initial_load.js'
 import Model from './src/js/model.js'
 import View from './src/js/view.js'
+import inputs from './src/lib/misc.js'
 
 window.onload = () => {
   init()
